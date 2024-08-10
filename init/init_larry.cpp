9b8e38fa028a4bca5c1ec54f900fdda7c58a21d0
Vivekachooz <vivekachooz@gmail.com>
2024-08-10 15:23:00 +0530

    larry: Setup variant handling based on sku
    Change-Id: I7465d96123c480ba9228a3c86fd52b8bbda17487

--- init/init_larry.cpp
+++ init/init_larry.cpp
@@ -9,23 +9,29 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
+#include <string>
+#include <vector>
+
 using android::base::GetProperty;
 
 /*
  * SetProperty does not allow updating read only properties and as a result
- * does not work for our use case. Write "OverrideProperty" to do practically
+ * does not work for our use case. Write "property_override" to do practically
  * the same thing as "SetProperty" without this restriction.
  */
-void OverrideProperty(const char* name, const char* value) {
-    size_t valuelen = strlen(value);
-
-    prop_info* pi = (prop_info*)__system_property_find(name);
+void property_override(const std::string& prop, const std::string& value, bool add = true) {
+    prop_info* pi = (prop_info*)__system_property_find(prop.c_str());
     if (pi != nullptr) {
-        __system_property_update(pi, value, valuelen);
-    } else {
-        __system_property_add(name, strlen(name), value, valuelen);
+        __system_property_update(pi, value.c_str(), value.length());
+    } else if (add) {
+        __system_property_add(prop.c_str(), prop.length(), value.c_str(), value.length());
     }
 }
+
+const std::vector<std::string> ro_props_default_source_order = {
+    "bootimage.", "odm.", "product.", "system.",
+    "system_ext.", "vendor.", "vendor_dlkm.", "",
+};
 
 /*
  * Only for read-only properties. Properties that can be wrote to more
@@ -33,16 +39,12 @@ void OverrideProperty(const char* name, const char* value) {
  * after the original property has been set.
  */
 void vendor_load_properties() {
-    auto prjname = std::stoi(GetProperty("ro.boot.prjname", "0"));
-
-    switch (prjname) {
-        case 22872: // IN
-            OverrideProperty("ro.product.product.model", "CPH2467");
-            break;
-        case 22871: // EU
-            OverrideProperty("ro.product.product.model", "CPH2465");
-            break;
-        default:
-            LOG(ERROR) << "Unexpected prjname: " << prjname;
+    auto sku = GetProperty("ro.boot.product.hardware.sku", "");
+    for (const auto& source : ro_props_default_source_order) {
+        if (sku == "2") { // IN
+            property_override("ro.product." + source + "model", "CPH2467");
+        } else if (sku == "6") { // EU
+            property_override("ro.product." + source + "model", "CPH2465");
+        }
     }
 }
